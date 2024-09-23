2029f2024e0508b38d77bcfa8b4e468fc20b6832
EwokOzwok <eozmat@albany.edu>
2024-09-23 12:59:28 -0400

    updated nav link

--- data-raw/navlinks.R
+++ data-raw/navlinks.R
@@ -1,3 +1,3 @@
 ## code to prepare `navlinks` dataset goes here
-navlinks<-readxl::read_excel("data-raw/navlinks.xlsx")
+navlinks<-readxl::read_excel("data-raw/Copy of navlinks.xlsx")
 usethis::use_data(navlinks, overwrite = TRUE)
