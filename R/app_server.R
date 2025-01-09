38a00123bdf2f2ebb52a6f6d0c773957085c2215
EwokOzwok <eozmat@albany.edu>
2025-01-09 10:21:19 -0500

    Spring 2025 updates (random nav order)

--- R/app_server.R
+++ R/app_server.R
@@ -8,10 +8,37 @@
 #' @noRd
 app_server <- function(input, output, session) {
 
+  Navigator_List<-c("Choose a Navigator",
+    "Melissa - (Mon, Thurs)",
+    "Evan - (Mon, Wed, Fri)",
+    "Chioma - (Tues, Thurs, Fri)",
+    "Meg - (Mon, Wed, Thurs)",
+    "Shea - (Tues, Thurs, Fri)",
+    "Clare - (Mon, Tues, Thurs)",
+    "Mikhaela - (Mon, Tues, Wed)")
+
+  first_item <- Navigator_List[1]
+  Randomized_Navs <- Navigator_List[-1]
+
+  shuffled_navs <- sample(Randomized_Navs)
+
+  randomized_list <- c(first_item, shuffled_navs)
+
+  print(randomized_list)
+
+  output$Randomized_Nav_List<-renderUI({
+    tagList(
+      f7SmartSelect("Navigators", label=NULL, placeholder = "Choose a Navigator",
+                    choices = randomized_list,
+                    selected = "Choose a Navigator", openIn = c("sheet"), searchbar = F, virtualList = F)
+    )
+  })
+
 
 
   observeEvent(input$Navigators, {
       SelectedNavigator<- input$Navigators
+      print(SelectedNavigator)
       SelectedNavLink<- NEW_navlinks[NEW_navlinks[,1]==SelectedNavigator,2]
       shortname<-NEW_navlinks[NEW_navlinks[,1]==SelectedNavigator,5]
       buttontext<- paste("Schedule with ", shortname, sep = "")
--- R/app_ui.R
+++ R/app_ui.R
@@ -47,17 +47,19 @@ app_ui <- function(request) {
                 hover = TRUE,
                 f7Card(
                   f7Align(h2("Select a Navigator to view their schedule:"), side = c("left")),
-                  f7SmartSelect("Navigators", label=NULL, placeholder = "Choose a Navigator",
-                           choices = c(
-                             "Choose a Navigator",
-                             "Melissa - (Mon, Thurs)",
-                             "Evan - (Mon, Wed, Fri)",
-                             "Chioma - (Tues, Thurs, Fri)",
-                             "Meg - (Mon, Wed, Thurs)",
-                             "Shea - (Tues, Thurs, Fri)",
-                             "Clare - (Mon, Tues, Thurs)",
-                             "Mikhaela - (Mon, Tues, Wed)"),
-                           selected = "Choose a Navigator", openIn = c("sheet"), searchbar = F, virtualList = F),
+                  uiOutput("Randomized_Nav_List"),
+
+                  # f7SmartSelect("Navigators", label=NULL, placeholder = "Choose a Navigator",
+                  #          choices = c(
+                  #            "Choose a Navigator",
+                  #            "Melissa - (Mon, Thurs)",
+                  #            "Evan - (Mon, Wed, Fri)",
+                  #            "Chioma - (Tues, Thurs, Fri)",
+                  #            "Meg - (Mon, Wed, Thurs)",
+                  #            "Shea - (Tues, Thurs, Fri)",
+                  #            "Clare - (Mon, Tues, Thurs)",
+                  #            "Mikhaela - (Mon, Tues, Wed)"),
+                  #          selected = "Choose a Navigator", openIn = c("sheet"), searchbar = F, virtualList = F),
 
 
                   # f7Picker("Navigators", label=NULL, placeholder = "Choose a Navigator",
